950062815862e47a56ebfe538bdfc1f6f37c7bc5
Abayomi Aishida <yomicorperate@gmail.com>
2021-11-27 10:26:41 +0100

    Mapping response from json

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
+import 'rxjs/add/operator/map'
 
 @Injectable()
 export class ProductService {
@@ -9,6 +10,6 @@ export class ProductService {
   }
 
   getAlbum(id:number) {
-    return this._http.get(this._albumUrl)
+    return this._http.get(this._albumUrl).map(response => { response.json() })
   }
 }
