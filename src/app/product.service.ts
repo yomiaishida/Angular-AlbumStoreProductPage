c91a01dd4d99cea5c5f9de9737f0104ed8c4c81c
Abayomi Aishida <yomicorperate@gmail.com>
2021-12-06 14:52:57 +0100

    importing album service in product service

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import 'rxjs/add/operator/map'
+import { Album } from './album'
 
 @Injectable()
 export class ProductService {
