416d42118b22912b79f86ec5d498a5722b20c03d
Abayomi Aishida <yomicorperate@gmail.com>
2021-12-08 09:09:53 +0100

    Creating navigation with angular router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+import { ProductListComponent } from './product-list/product-list.component';
 import { AppComponent } from './app.component';
 import { ProductPageComponent } from './product-page/product-page.component';
 import { ProductDescriptionComponent } from './product-description/product-description.component';
 import { ProductService } from './product.service';
 import { ProductTracklistingComponent } from './product-tracklisting/product-tracklisting.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const appRoutes: Routes = [
+  {path: 'products', component: 'ProductListComponent'},
+  {path: 'product/:id', component: 'ProductPageComponent'},
+  {path: '', redirectTo: 'products', pathMatch: 'full'}
+]
 
 @NgModule({
   declarations: [
@@ -19,7 +26,8 @@ import { ProductTracklistingComponent } from './product-tracklisting/product-tra
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [ProductService],
   bootstrap: [AppComponent]
