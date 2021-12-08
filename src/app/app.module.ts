4fd4447a3ea693ed176f5b90450f73d3378caa4e
Abayomi Aishida <yomicorperate@gmail.com>
2021-12-08 09:16:16 +0100

    Fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,8 +11,8 @@ import { ProductTracklistingComponent } from './product-tracklisting/product-tra
 import { RouterModule, Routes } from '@angular/router';
 
 const appRoutes: Routes = [
-  {path: 'products', component: 'ProductListComponent'},
-  {path: 'product/:id', component: 'ProductPageComponent'},
+  {path: 'products', component: ProductListComponent},
+  {path: 'product/:id', component: ProductPageComponent},
   {path: '', redirectTo: 'products', pathMatch: 'full'}
 ]
 
